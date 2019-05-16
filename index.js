60b7256cb34f603d295c5f106eb4fe45cc2ff49e
Huong <huongnguyenak96@gmail.com>
2019-05-16 20:25:03 +0700

    update dest

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ 
+    dest: 'public/images/'
+})
 const app = express();
 app.listen(3000)
 app.set('view engine', 'ejs');
 
 app.get('/upload-file',(req,res)=>{
     res.render('form');
+})
+app.post('/upload-file',
+upload.single('avatar'),
+(req,res)=>{
+    const avatar = req.file
+    const name = req.body.txtName;
+    res.send({ name, avatar })
 })
